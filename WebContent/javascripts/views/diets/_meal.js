f7ca7c65c796196e7c8ca40d791a7c6442d127f8
Denis Rangel <denis.rangel@gmail.com>
2013-03-28 10:14:19 -0300

    remove nature ion new diets

--- WebContent/javascripts/views/diets/_meal.js
+++ WebContent/javascripts/views/diets/_meal.js
@@ -3,16 +3,21 @@ define([
   'jquery-ui',
   'underscore', 
   'backbone', 
-  'models/patient',
-  'models/period',
-  'models/type',
-  'collections/meals', 
-], function($, UI, _, Backbone) {
+  'models/nature', 
+  'text!../../templates/diets/nature.html', 
+], function($, UI, _, Backbone, Nature, home) {
 	return Backbone.View.extend({
 		tagName: 'li',
 		className: 'ui-widget-content', 
+		events: {
+			'click a': 'remove'
+		},
 		render: function() {
-			return $(this.el).text(this.model.get('dish').period + "h" + " (" + this.model.get('dish').nature.description + ")");
+			return $(this.el).html(_.template(home, {"model": this.model}));
+		},
+		remove: function(ev) {
+			this.model.get('dish').nature = (new Nature({description: '+'})).toJSON();
+			this.render();
 		}
 	});
 });
