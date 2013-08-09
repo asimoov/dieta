ffee6d9e142f902d7a85221b4b2bb73137e0120a
Denis Rangel <denis.rangel@gmail.com>
2013-08-09 09:15:32 -0300

    home refactory and add glow menu

--- WebContent/javascripts/views/home.js
+++ WebContent/javascripts/views/home.js
@@ -7,7 +7,12 @@ define([
 	return Backbone.View.extend({
 		template: _.template(home),
 		render : function() {
+			this.makeSelector();
 			this.$el.append(this.template());
+		},
+		makeSelector: function() {
+			$("li.active").removeClass("active");
+			$("a[href~='#']").parent().addClass("active");
 		},
 		close: function() {
 			this.$el.unbind().empty();
--- WebContent/javascripts/views/interments/interments.js
+++ WebContent/javascripts/views/interments/interments.js
@@ -11,6 +11,7 @@ define([
 			this.$el.append(home);
 			
 			console.time("interments");
+			this.makeSelector();
 			var frag = document.createDocumentFragment();
 			this.collection.each(function(interment) {
 				var intermentView = new IntermentView({model: interment, ward: this.options.ward, "selected": this.options.selected, root: this.options.root});
@@ -22,6 +23,13 @@ define([
 			
 			$("ul", this.$el).append(frag);
 			console.timeEnd("interments");
+		},
+		makeSelector: function() {
+			$("li.active").removeClass("active");
+			
+			if(this.options.ward !== undefined) {
+				$("a[href^='#"+ this.options.ward.url() +"/interments']").parent().addClass("active");
+			}
 		},
 		close: function() {
 			_.forEach(this.subviews, function(subview){
